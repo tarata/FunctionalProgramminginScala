3b3f40a96667c2b945e7b8d549bdc91b2c727b36
Arata Tanaka <tarata43@yahoo.co.jp>
2015-06-23 13:31:01 +0900

    p9まで

--- src/main/scala/p1/Cafe.scala
+++ src/main/scala/p1/Cafe.scala
@@ -1,10 +1,28 @@
 package p1
 
+object Cafe {
+  def coalesce(charges: Seq[Charge]): Seq[Charge] = charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toSeq
+}
+
+case class Charge(cc:CreditCard, amount:Double) {
+  def combine(other:Charge): Charge =
+    if(cc == other.cc) {
+      copy(amount = amount + other.amount)
+    } else {
+      throw new Exception("Cannot combine charges to different cards")
+    }
+}
+
 class Cafe {
-  def buyCoffee(cc:CreditCard): Coffee = {
+  def buyCoffee(cc:CreditCard): (Coffee, Charge) = {
     val cup = new Coffee()
-    cc.charge(cup.price)
-    cup
+    (cup, Charge(cc, cup.price))
+  }
+
+  def buyCoffees(cc:CreditCard, n:Int): (Seq[Coffee], Charge) = {
+    val purchases: Seq[(Coffee,Charge)] = List.fill(n)(buyCoffee(cc))
+    val (coffees, charges) = purchases.unzip
+    (coffees, charges.reduce((c1, c2) => c1.combine(c2)))
   }
 }
 
