1429f6910b9773ce1782169c814a8741eb0eac95
Arata Tanaka <tarata43@yahoo.co.jp>
2015-07-08 19:43:37 +0900

    高階関数

--- src/main/scala/gettingstarted/GettingStarted.scala
+++ src/main/scala/gettingstarted/GettingStarted.scala
@@ -1,16 +1,14 @@
 package gettingstarted
 
 object MyModule {
-  def abs(n:Int):Int = if(n < 0) -n else n
-
-  private def formatAbs(x :Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
 
   def main (args: Array[String]) {
     println(formatAbs(-1))
+    println(formatFactorial(7))
   }
+
+  def abs(n:Int):Int = if(n < 0) -n else n
+
 
   def factorial(n:Int): Int = {
     @annotation.tailrec
@@ -30,4 +28,14 @@ object MyModule {
 
     go(n, 0, 1)
   }
+
+  private def formatAbs(x :Int) = formatResult("absolute", x, abs)
+
+  private def formatFactorial(n: Int) = formatResult("factorial", n ,factorial)
+
+  def formatResult(name:String, n:Int, f:Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
 }
