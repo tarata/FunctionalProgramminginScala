2629964bdf02ccc547f67842ff4b7b1a33905b84
Arata Tanaka <tarata43@yahoo.co.jp>
2015-11-13 21:18:57 +0900

    Q 3.26

---
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -0,0 +1,22 @@
+package fpinscala.datastructures
+
+sealed trait Tree[+A] {
+  def size: Int
+}
+
+case class Leaf[A](value: A) extends Tree[A] {
+  override def size: Int = 1
+}
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
+  override def size: Int = left.size + right.size + 1
+}
+
+object Tree {
+  def maximum(tree: Tree[Int]): Int = {
+    tree match {
+      case Leaf(v) => v
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+  }
+}
