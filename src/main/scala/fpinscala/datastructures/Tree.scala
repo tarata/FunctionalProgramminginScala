aa95d597d23acba574ec1413d93f90a00f61bad6
Arata Tanaka <tarata43@yahoo.co.jp>
2015-11-13 21:22:15 +0900

    Q 3.27

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -2,14 +2,19 @@ package fpinscala.datastructures
 
 sealed trait Tree[+A] {
   def size: Int
+  def depth: Int
 }
 
 case class Leaf[A](value: A) extends Tree[A] {
   override def size: Int = 1
+
+  override def depth: Int = 1
 }
 
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
   override def size: Int = left.size + right.size + 1
+
+  override def depth: Int = (left.depth max right.depth) + 1
 }
 
 object Tree {
