c4e1266f2355bb1d1c469840314e4733d0876797
Arata Tanaka <tarata43@yahoo.co.jp>
2015-11-13 22:21:11 +0900

    [WIP]Q 3.29

--- src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -22,9 +22,40 @@ class TreeSpec extends FreeSpec with Matchers {
   }
 
   "Exercise 3.27" - {
-    "the depth should be 2" in {
-      Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).depth shouldBe 2
+    "the depth should be 3" in {
+      Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).depth shouldBe 3
     }
   }
 
+  "Exercise 3.28" - {
+    "map should be applied" in {
+      Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).map(_ + 1) shouldBe Branch(Branch(Leaf(2), Leaf(3)), Leaf(4))
+    }
+  }
+
+  "Exercise 3.29" - {
+    "the size2 of Leaf is 1" in {
+      Leaf(1).size2 shouldBe 1
+    }
+
+    "the size2 of Leaf with Leaf is 3" in {
+      Branch(Leaf(1), Leaf(2)).size2 shouldBe 3
+    }
+
+    "the depth2 should be 1" in {
+      Leaf(1).depth2 shouldBe 1
+    }
+
+    "the depth2 should be 2" in {
+      Branch(Leaf(1), Leaf(2)).depth2 shouldBe 2
+    }
+
+    "the depth2 should be 3" in {
+      Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).depth2 shouldBe 3
+    }
+
+    "the depth2 should be 4" in {
+      Branch(Branch(Branch(Leaf(1), Leaf(2)), Leaf(4)), Leaf(3)).depth2 shouldBe 4
+    }
+  }
 }
