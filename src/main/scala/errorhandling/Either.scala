a980e6d3739af1f16a0a11ca07072bce3065e37f
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-09 15:28:17 +0900

    Ex 4.7

--- src/main/scala/errorhandling/Either.scala
+++ src/main/scala/errorhandling/Either.scala
@@ -22,6 +22,18 @@ sealed trait Either[+E, +A] {
   }
 }
 
+object Either {
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+    case Nil => Right(Nil)
+    case (h::t) => h.flatMap(a => sequence(t).map(a :: _))
+  }
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case Nil => Right(Nil)
+    case (h :: t) => f(h).flatMap(hh => traverse(t)(f).map(hh :: _))
+  }
+}
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 
 case class Right[+A](value: A) extends Either[Nothing, A]
