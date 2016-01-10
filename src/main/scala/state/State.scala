63b3bbb680803cf0cb5078d104c59ef7c6fa4b4d
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-11 00:33:46 +0900

    Ex 6.1

---
+++ src/main/scala/state/State.scala
@@ -0,0 +1,22 @@
+package state
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+object RNG {
+  def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
+    case (i, r) if i < 0 => (- (i + 1), r)
+    case (i, r) => (i, r)
+  }
+}
+
