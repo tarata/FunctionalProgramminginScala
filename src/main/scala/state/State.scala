02736be5f56080309a6657cb4e5a46b40a4941a3
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-11 00:59:10 +0900

    Ex 6.6

--- src/main/scala/state/State.scala
+++ src/main/scala/state/State.scala
@@ -25,6 +25,12 @@ object State {
     (f(a), rng2)
   }
 
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng2)
+    (f(a, b), rng3)
+  }
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
     case (i, r) if i < 0 => (- (i + 1), r)
     case (i, r) => (i, r)
