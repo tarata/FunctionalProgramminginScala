70e0e2efc943dacd46d1b1530dae778a91473f64
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-11 00:39:27 +0900

    Ex 6.3

--- src/main/scala/state/State.scala
+++ src/main/scala/state/State.scala
@@ -23,5 +23,26 @@ object RNG {
     val (i, r) = nonNegativeInt(rng)
     (i.toDouble / Int.MaxValue, r)
   }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = nonNegativeInt(rng)
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG):((Double, Int), RNG) = {
+    val (d, r1) = double(r1)
+    val (i, r2) = nonNegativeInt(rng)
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG)  = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1,d2,d3), r3)
+  }
+
+
 }
 
