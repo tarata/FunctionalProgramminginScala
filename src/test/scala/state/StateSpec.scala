4a092c94477ac3be13079f7774b452861ebcce45
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-11 01:16:42 +0900

    Ex 6.7

--- src/test/scala/state/StateSpec.scala
+++ src/test/scala/state/StateSpec.scala
@@ -17,4 +17,18 @@ class StateSpec extends FreeSpec with Matchers {
       actual.toSet.size shouldBe 10
     }
   }
+  "ints2" - {
+    "should return Nil when count = 0" in {
+      val simpleRng = SimpleRNG(1)
+      val (actual, r) = State.ints2(0)(simpleRng)
+      actual shouldBe Nil
+    }
+
+    "should return List which has 10 different int values" in {
+      val simpleRng = SimpleRNG(1)
+      val (actual, r) = State.ints2(10)(simpleRng)
+      actual.size shouldBe 10
+      actual.toSet.size shouldBe 10
+    }
+  }
 }
