b61d4fdd7a03c6155d2763ca1eabe2f911cf1548
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-16 14:36:42 +0900

    Fix interface for Ex8.3

--- src/main/scala/testing/MyGen.scala
+++ src/main/scala/testing/MyGen.scala
@@ -1,14 +1,23 @@
 package testing
 
-trait MyGen[A]
+
+object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+}
 
 trait Prop {
+  import Prop._
   def &&(p: Prop): Prop = new Prop {
-    def check: Boolean = Prop.this.check && p.check
+    def check: Either[(FailedCase, SuccessCount), SuccessCount] = for {
+      s1 <- Prop.this.check.right
+      s2 <- p.check.right
+    } yield s1 + s2
   }
 
-  def check: Boolean
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 }
+
 
 object MyGen {
   def listOf[A](a: MyGen[A]): MyGen[List[A]] = ???
@@ -17,3 +26,5 @@ object MyGen {
 
   def forAll[A](a: MyGen[A])(f: A => Boolean): Prop = ???
 }
+
+trait MyGen[A]
