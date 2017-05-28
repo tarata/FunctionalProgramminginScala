d3631785252376fa540e520863600c04f097135c
Arata Tanaka <tarata43@yahoo.co.jp>
2017-05-28 22:47:46 +0900

    10.4, 10.5(pass)

--- src/main/scala/monoids/Monoid.scala
+++ src/main/scala/monoids/Monoid.scala
@@ -1,9 +1,26 @@
 package monoids
 
+/**
+  * モノイドとは: 型Aと、法則を満たすMonoid[A]の実装のこと
+  * 型とその型に対する2項演算opをあわせたもので、結合律を満たし単位元zeroを持つもののこと
+  * @tparam A
+  */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
 
   def zero: A
 
 
+
+
+}
+
+object Monoid {
+    /**
+    * 畳み込む総称関数
+    * @tparam A
+    */
+  def concatenate[A](as: List[A], m: Monoid[A]): A = as.foldLeft(m.zero)(m.op)
+
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.map(f).foldLeft(m.zero)(m.op)
 }
