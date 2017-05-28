f43b8ab0cec6df1437f67bf7df5cf3f0d7bbd6d6
Arata Tanaka <tarata43@yahoo.co.jp>
2017-05-28 22:56:06 +0900

    10.10, 10.11は10.7をやってからやるとイメージつきそう

--- src/main/scala/monoids/Monoid.scala
+++ src/main/scala/monoids/Monoid.scala
@@ -23,4 +23,19 @@ object Monoid {
   def concatenate[A](as: List[A], m: Monoid[A]): A = as.foldLeft(m.zero)(m.op)
 
   def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.map(f).foldLeft(m.zero)(m.op)
+
+  def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = ???
+
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    override def op(a1: WC, a2: WC): WC = ???
+
+    override def zero: WC = ???
+  }
 }
+
+sealed trait WC
+
+case class Stub(chars: String) extends WC
+
+case class Part(lStub: String, words: Int, rStub: String) extends WC
+
